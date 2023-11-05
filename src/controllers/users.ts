877b5c65f4871e09c8deead6e707622165293b13
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-11-05 18:16:28 +0700

    init project

---
+++ src/controllers/users.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from 'express';
+
+export const getUser = async (req: Request, res: Response) => {
+  res.send({});
+};
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import bodyParser from 'body-parser';
+import express, { Express } from 'express';
+import morgan from 'morgan';
+import { startServer } from 'server';
+
+import setupRoutes from './routers';
+
+require('dotenv-flow').config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const routes = setupRoutes();
+routes.forEach(({ path, router }) => {
+  app.use(path, router);
+});
+
+startServer();
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running at http://localhost:${port}`);
+});
